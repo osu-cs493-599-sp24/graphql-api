d4163d0c00f359a5ea108521164f5981bcf17bed
Rob Hess <hessro@oregonstate.edu>
2024-06-04 09:38:02 -0700

    Add forgotten getUserById() function.

--- models/user.js
+++ models/user.js
@@ -21,3 +21,19 @@ exports.insertNewUser = async function (user) {
     })
     return result.insertedId
 }
+
+/*
+ * Fetch a User from the DB based on ID.
+ */
+exports.getUserById = async function (id) {
+    const db = getDb()
+    const collection = db.collection('users')
+    if (!ObjectId.isValid(id)) {
+        return null
+    } else {
+        const results = await collection
+            .find({ _id: new ObjectId(id) })
+            .toArray()
+        return results[0]
+    }
+}
